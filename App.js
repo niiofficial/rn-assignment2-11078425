1b9d29192a29ed577f8df998d0e9afe6ca492dfa
niiofficial <rtackie007@st.ug.edu.gh>
2024-05-26 17:12:48 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is <Text style={styles.name}>Richmond Tackie</Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+});
